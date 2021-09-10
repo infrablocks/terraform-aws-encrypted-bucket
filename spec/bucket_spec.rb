d3a471d27713d9e8320369f5c3bbc8793fb72356
jonassvalin <jonassvalin@gmail.com>
2021-09-10 16:50:16 +0200

    Add logging

--- spec/bucket_spec.rb
+++ spec/bucket_spec.rb
@@ -215,8 +215,7 @@ describe 'Encrypted bucket' do
   context 'with kms_key_arn' do
     before(:all) do
       puts
-      puts "kms_key_arn: "
-      puts output_for(:prerequisites, 'kms_key_arn')
+      puts "kms_key_arn: #{output_for(:prerequisites, 'kms_key_arn')}"
       puts
 
       provision(kms_key_arn: output_for(:prerequisites, 'kms_key_arn'))
